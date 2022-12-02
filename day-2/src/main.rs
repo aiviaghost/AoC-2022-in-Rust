abfecd98d51e1c5b5955ef90195ccb2180c69882
aiviaghost <smurfplatta@gmail.com>
2022-12-02 12:00:11 +0100

    Refactor day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,48 +1,50 @@
 use std::fs;
 
-fn solve_1(input: Vec<String>) {
-    let ans: i32 = input
+fn get_char_pairs(input: Vec<String>) -> Vec<(char, char)> {
+    input
         .iter()
         .map(|line| line.split(" ").map(|x| x.as_bytes()))
         .map(|mut t| {
             let a = t.next().unwrap()[0] as char;
             let b = t.next().unwrap()[0] as char;
-            match (a, b) {
-                ('A', 'X') => 1 + 3,
-                ('A', 'Y') => 2 + 6,
-                ('A', 'Z') => 3 + 0,
-                ('B', 'X') => 1 + 0,
-                ('B', 'Y') => 2 + 3,
-                ('B', 'Z') => 3 + 6,
-                ('C', 'X') => 1 + 6,
-                ('C', 'Y') => 2 + 0,
-                ('C', 'Z') => 3 + 3,
-                (_, _) => 0,
-            }
+            (a, b)
+        })
+        .collect()
+}
+
+fn solve_1(input: Vec<String>) {
+    let ans: i32 = get_char_pairs(input)
+        .iter()
+        .map(|(a, b)| match (a, b) {
+            ('A', 'X') => 1 + 3,
+            ('A', 'Y') => 2 + 6,
+            ('A', 'Z') => 3 + 0,
+            ('B', 'X') => 1 + 0,
+            ('B', 'Y') => 2 + 3,
+            ('B', 'Z') => 3 + 6,
+            ('C', 'X') => 1 + 6,
+            ('C', 'Y') => 2 + 0,
+            ('C', 'Z') => 3 + 3,
+            (_, _) => 0,
         })
         .sum();
     println!("{ans}");
 }
 
 fn solve_2(input: Vec<String>) {
-    let ans: i32 = input
+    let ans: i32 = get_char_pairs(input)
         .iter()
-        .map(|line| line.split(" ").map(|x| x.as_bytes()))
-        .map(|mut t| {
-            let a = t.next().unwrap()[0] as char;
-            let b = t.next().unwrap()[0] as char;
-            match (a, b) {
-                ('A', 'X') => 0 + 3,
-                ('A', 'Y') => 3 + 1,
-                ('A', 'Z') => 6 + 2,
-                ('B', 'X') => 0 + 1,
-                ('B', 'Y') => 3 + 2,
-                ('B', 'Z') => 6 + 3,
-                ('C', 'X') => 0 + 2,
-                ('C', 'Y') => 3 + 3,
-                ('C', 'Z') => 6 + 1,
-                (_, _) => 0,
-            }
+        .map(|(a, b)| match (a, b) {
+            ('A', 'X') => 0 + 3,
+            ('A', 'Y') => 3 + 1,
+            ('A', 'Z') => 6 + 2,
+            ('B', 'X') => 0 + 1,
+            ('B', 'Y') => 3 + 2,
+            ('B', 'Z') => 6 + 3,
+            ('C', 'X') => 0 + 2,
+            ('C', 'Y') => 3 + 3,
+            ('C', 'Z') => 6 + 1,
+            (_, _) => 0,
         })
         .sum();
     println!("{ans}");
