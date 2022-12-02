03c4b56a0a62ededdacc05bf98365e0320c3e1eb
aiviaghost <smurfplatta@gmail.com>
2022-12-02 14:03:13 +0100

    Refactor day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,28 +1,24 @@
 use std::fs;
 
-fn solve_1(input: Vec<String>) {
-    let ans: i32 = input
+fn compute_calories_per_elf(input: Vec<String>) -> Vec<i32> {
+    input
         .iter()
         .map(|x| {
             x.split_terminator("\n")
                 .map(|i| i.parse::<i32>().unwrap())
                 .sum()
         })
-        .max()
-        .unwrap();
+        .collect()
+}
+
+fn solve_1(input: Vec<String>) {
+    let ans = *compute_calories_per_elf(input).iter().max().unwrap();
 
     println!("{ans}");
 }
 
 fn solve_2(input: Vec<String>) {
-    let mut xs = input
-        .iter()
-        .map(|x| {
-            x.split_terminator("\n")
-                .map(|i| i.parse::<i32>().unwrap())
-                .sum()
-        })
-        .collect::<Vec<i32>>();
+    let mut xs = compute_calories_per_elf(input);
     xs.sort();
     let ans: i32 = xs.iter().rev().take(3).sum();
     println!("{ans}");
